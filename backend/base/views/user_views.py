0ebc668b1018a8ce101a57ee24e517789f78b258
MohammadJavad14 <mrph14@gmail.con>
2021-06-26 16:46:08 +0430

    Backend authentication part-2

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,24 +1,17 @@
-
 from django.contrib.auth.models import User
 from django.core.checks import messages
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework import serializers
 from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .models import Product
-from .products import products
-from .serializers import ProductSerializer , UserSerializer , UserSerializerWithToken
+from base.serializers import ProductSerializer , UserSerializer , UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
-
-# Create your views here.
-
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -70,17 +63,4 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProduct(request,pk):
-    product= Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
